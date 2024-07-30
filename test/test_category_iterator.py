e95f53bd515bc2ab48988bbc5d6f8b49e5b07255
Georgii Ovechkin <goqwertys@gmail.com>
2024-07-30 23:38:26 +0400

    list test for CategoryIterator added and passed

---
+++ test/test_category_iterator.py
@@ -0,0 +1,11 @@
+from src.models import Product, Category, CategoryIterator
+
+
+def test_category_iterator_returns_all_products(cars):
+    pr1 = Product("Ford T", "American classic", 100000.0, 5)
+    pr2 = Product("Volkswagen Juke", "GOAT", 150000.0, 2)
+    cars = Category("Cars", "Classic")
+    cars.add_product(pr1)
+    cars.add_product(pr2)
+    iterator = CategoryIterator(cars)
+    assert list(iterator) == [pr1, pr2]
