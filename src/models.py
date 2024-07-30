bc91facf6ee3595b79adb884ac6bf71ac9002113
Georgii Ovechkin <goqwertys@gmail.com>
2024-07-30 22:53:54 +0400

    __len__ redefined in Product

--- src/models.py
+++ src/models.py
@@ -68,6 +68,9 @@ class Product:
     def __str__(self):
         return f"{self.name}, {self.__price} руб. Остаток: {self.count} шт."
 
+    def __len__(self):
+        return self.count
+
 
 class Category:
     """Represents a category of products"""
