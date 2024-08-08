18409218a7e39001abbc9fd32c5baed5ed1bfd32
Georgii Ovechkin <goqwertys@gmail.com>
2024-08-08 06:15:48 +0400

    LoggingMixin added

--- src/logging_mixin.py
+++ src/logging_mixin.py
@@ -1,4 +1,4 @@
 class LoggingMixin:
     def __repr__(self):
-        attrs = ', '.join(self.__dict__.values())
+        attrs = ', '.join(str(self.__dict__.values()))
         return f"{self.__class__.__name__}: ({attrs})"
