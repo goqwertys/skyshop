727371bb12629e3e4b3773a3989bac1bf3f03f5e
Georgii Ovechkin <goqwertys@gmail.com>
2024-08-08 06:27:50 +0400

    __repr__ tests for LoggingMixin fixed

--- test/test_products_repr.py
+++ test/test_products_repr.py
@@ -6,8 +6,8 @@ def test_mixin_product_repr(product):
 
 
 def test_mixin_smartphone_repr(smartphone_1):
-    assert repr(smartphone_1) == "SmartPhone: ('Smartphone', 'Description', '500.0', 2, 800, 'ModelX', 128, 'Black')"
+    assert repr(smartphone_1) == "SmartPhone: ('Smartphone', 'Description', 500.0, 2, 800, 'ModelX', 128, 'Black')"
 
 
 def test_mixin_lawn_grass_repr(lawn_grass_1):
-    assert repr(lawn_grass_1) == "LawnGrass: ('LawnGrass', 'Description', '20.0', 10, 'USA', 'Spring', 'Green')"
+    assert repr(lawn_grass_1) == "LawnGrass: ('LawnGrass', 'Description', 20.0, 10, 'USA', 'Spring', 'Green')"
