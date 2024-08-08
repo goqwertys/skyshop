079a8df3bc8213cfb8b43cbf36afc4091818617a
Georgii Ovechkin <goqwertys@gmail.com>
2024-08-08 06:28:30 +0400

    LoggingMixin.__repr__ redefined / test passed

--- src/logging_mixin.py
+++ src/logging_mixin.py
@@ -1,4 +1,12 @@
 class LoggingMixin:
     def __repr__(self):
-        attrs = ', '.join(str(self.__dict__.values()))
+        def format_value(value):
+            if isinstance(value, str):
+                return f"'{value}'"
+            elif isinstance(value, tuple):
+                return str(value[0])
+            else:
+                return str(value)
+
+        attrs = ', '.join(format_value(value) for value in self.__dict__.values())
         return f"{self.__class__.__name__}: ({attrs})"
